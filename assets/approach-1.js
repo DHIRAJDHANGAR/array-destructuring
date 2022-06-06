4c19e1b84a76ddedd86e41bd8493f0fedb498f97
DHIRAJDHANGAR <dhadpe05@gmail.com>
2022-06-06 13:31:47 +0530

    first commit

---
+++ assets/approach-1.js
@@ -0,0 +1,91 @@
+const users = [
+    {
+      id: 123,
+      name: "Sagar",
+      city: "Indore",
+    },
+    {
+      id: 124,
+      name: "Arun",
+      city: "Morena",
+    },
+    {
+      id: 125,
+      name: "Nikhil",
+      city: "Dhule",
+    },
+  ];
+
+  let users_1 = [];
+  for(i=0; i<users.length; i++){
+    let {id, name} = users[i];
+    let usersArr = {usersId : id, usersName : name , order :[]};
+    users_1.push(usersArr);
+  }
+// console.log(users_1);
+
+  const orders = [
+    {
+      id: 1234,
+      name: "iPhone 13 Pro Max",
+      price: "123000",
+      customerId: 123,
+    },
+    {
+      id: 1235,
+      name: "Apple iWatch",
+      price: "49000",
+      customerId: 124,
+    },
+  ];
+
+  const orders_1 = [];
+  for(j=0; j<orders.length; j++){
+    let {id, name, price } = orders[j];
+    let ordersArr = {ordersId : id, productName : name, productPrice: price};
+    orders_1.push(ordersArr);
+  }
+// console.log(orders_1);
+
+
+  const result = [
+    {
+      userId: 123,
+      userName: "Sagar",
+      orders: [
+        {
+          orderId: 1234,
+          productName: "iPhone 13 Pro Max",
+          productPrice: "123000",
+        },
+      ],
+    },
+    {
+      userId: 124,
+      userName: "Arun",
+      orders: [
+        {
+          orderId: 1235,
+          productName: "Apple iWatch",
+          productPrice: "49000",
+        },
+      ],
+    },
+    {
+      userId: 125,
+      userName: "Nikhil",
+      orders: [],
+    },
+  ];
+
+
+let result_1 =[];
+for(i=0; i<users.length; i++)
+for(j=0; j<orders.length; j++){
+    if(users[i].id === orders[j].customerId){
+        users_1[i].order= [orders_1[j]];
+        // console.log("ln1000", users_1)
+        result_1 = [...users_1];
+    }
+}
+console.log(result_1);
